7feabeb9c5eda7e38c07b84f08aa8c9da54c13fc
Dylan <secretplop@gmail.com>
2018-10-27 23:00:08 +0100

    dialogue improvements

--- law.js
+++ law.js
@@ -4,17 +4,22 @@ var audioBattle = new Audio("battle3.mp3");
 
 var ready = false;
 
+var dialouge;
+
 window.addEventListener("load",function(){
 	audioViolated.play();
 	ready = true;
+	dialogue = document.getElementById("dialogue");
 });
 window.addEventListener("click", function(){
 	if (ready == true) {
 		ready = false;
 		audioViolated.pause();
 		audioBlood.play();
+		dialogue.innerHTML = "Then Pay with your blood!"
 	}
 });
 audioBlood.addEventListener("ended", function(){
 	audioBattle.play();
+	dialogue.innerHTML = "";
 });
